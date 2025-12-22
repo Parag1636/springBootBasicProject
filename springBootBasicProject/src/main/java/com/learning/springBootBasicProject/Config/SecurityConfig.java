412e8bf78638c750227b88899d9aad8c88e70629
Parag Kushwah <kushwahparag1636@gmail.com>
2025-12-22 16:01:51 +0530

    Spring Security Added endpoints Authorize

--- springBootBasicProject/src/main/java/com/learning/springBootBasicProject/Config/SecurityConfig.java
+++ springBootBasicProject/src/main/java/com/learning/springBootBasicProject/Config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/employee/**").authenticated()
+                .antMatchers("/employee/**", "/user/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
